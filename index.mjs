e5bf0983f2404ff8fba5e26d289e9e743e10dbbc
Bristlefrost <me@bristlefrost.net>
2024-07-22 14:21:43 +0300

    Initial commit

---
+++ index.mjs
@@ -0,0 +1,27 @@
+import js from '@eslint/js';
+
+export default [
+	js.configs.recommended, // Enable all recommended rules
+
+	{
+		rules: {
+			'no-constructor-return': 'error',
+			'no-duplicate-imports': 'warn',
+			'no-promise-executor-return': 'error',
+			'no-self-compare': 'error',
+			'no-use-before-define': 'error',
+			'require-atomic-updates': 'warn',
+
+			'no-eval': 'error',
+			'no-implied-eval': 'error',
+
+			'eqeqeq': 'error',
+
+			'func-style': ['error', 'declaration', { 'allowArrowFunctions': true }],
+			'no-var': 'error',
+			'prefer-const': 'warn',
+
+			'yoda': 'error',
+		},
+	},
+];
